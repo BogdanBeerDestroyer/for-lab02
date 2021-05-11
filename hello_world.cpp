4029eca118ff0639148e69e9a36c38a18b85ad65
bogdan <bshumilishsky@mail.ru>
2021-05-12 00:06:15 +0300

    hello_world.cpp was corrected

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,9 +1,8 @@
 #include <iostream>
-
-using namespace std;
+#include <string>
 
 int main() {
-    string name;
-    cin >> name;
-    cout << "Hello, world from" << name;
+    std::string name;
+    std::cin >> name;
+    std::cout << "Hello, world from" << name;
 }
